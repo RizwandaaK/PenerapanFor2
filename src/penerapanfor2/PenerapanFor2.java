c428c2ddf10089f82da4298721d7a8911d2048b3
RizwandaaK <rizwanda_keysha_29rpl@student.smktelkom-mlg.sch.id>
2021-01-14 22:00:43 +0700

    PenerapanFor2

---
+++ src/penerapanfor2/PenerapanFor2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package penerapanfor2;
+
+/**
+ *
+ * @author rizwa
+ */
+public class PenerapanFor2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++){
+            for (int j = 1; j <= i; j++){
+                System.out.print("@ ");
+            }System.out.println("");
+    }    
+    }
+}
